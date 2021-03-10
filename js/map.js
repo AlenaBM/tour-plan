7673a0afeb0ea11fe0dfd877d70d7219e8199768
AlenaBM <9642444600a@gmail.com>
2021-03-10 12:20:58 +0300

    create js API map

---
+++ js/map.js
@@ -0,0 +1,31 @@
+ymaps.ready(init);
+      function init() {
+        var myMap = new ymaps.Map("map", {
+          center: [7.890703, 98.294772],
+          zoom: 17,
+        });
+        var myGeoObject = new ymaps.GeoObject({
+          geometry: {
+            type: "Point",
+            coordinates: [7.890703, 98.294772],
+          },
+        });
+        myMap.geoObjects.add(myGeoObject);
+
+        var myPlacemark = new ymaps.Placemark(
+          myMap.getCenter(),
+          {
+            balloonContentBody: [
+              "<address>",
+              "<strong>DoubleTree by Hilton Phuket Banthai Resort</strong>",
+              "<br/>",
+              "Thailand, Phuket, Ka Thu, Patong city",
+              "<br/>",
+              "</address>",
+            ].join(""),
+          },
+          
+        );
+
+        myMap.geoObjects.add(myPlacemark);
+      }
